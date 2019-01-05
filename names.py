5a0c616fddabf6a8e2e192ff48f55faa9c9aa1a7
Joe Bender <joebenderweb@gmail.com>
2019-01-05 12:28:51 -0500

    clean up unused files

--- names.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('names2017.csv', header=None, index_col=False)
-names = data.iloc[:, 0]
-names = list(names)
-names = [name for name in names if len(name) == 6]
-print(len(names))
